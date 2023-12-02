04fad4d58fd1613adc3572b78f8627b4c4d6ab25
Tom <tom963741@gmail.com>
2023-12-02 10:48:13 +0100

    add new version for part one, using not Regex

--- 01/Day01.java
+++ 01/Day01.java
@@ -7,12 +7,12 @@ import java.util.regex.Pattern;
 class Day01 {
   public static void main(String[] args) throws IOException {
     BufferedReader file = new BufferedReader(new FileReader("input.txt"));
-    System.out.println("Result part one : " + firstPart(file));
+    System.out.println("Result part one : " + firstPartWithRegex(file));
     System.out.println("Result part two : " + secondPart(file));
     file.close();
   }
 
-  public static Integer firstPart(BufferedReader file) throws IOException {
+  public static Integer firstPartWithRegex(BufferedReader file) throws IOException {
     Pattern oneIntPattern = Pattern.compile("·*\\d");
     Pattern twoIntsPattern = Pattern.compile("^\\D*(\\d|one|two|three|four|five|six|seven|eight|nine).*(\\d)\\D*$");
     Integer result = 0;
@@ -21,18 +21,32 @@ class Day01 {
     while((line = file.readLine()) != null) {
       Matcher oneIntMatcher = oneIntPattern.matcher(line);
       Matcher twoIntsMatcher = twoIntsPattern.matcher(line);
-      // System.out.println("-----------------------------");
-      // System.out.println(line);
-      
+
       if (twoIntsMatcher.matches()) {
         result += Integer.parseInt(twoIntsMatcher.group(1) + twoIntsMatcher.group(2));
-        // System.out.println(twoIntsMatcher.group(1) + twoIntsMatcher.group(2));
       } else if (oneIntMatcher.find()) {
         result += Integer.parseInt(oneIntMatcher.group()+oneIntMatcher.group());
-        // System.out.println(oneIntMatcher.group()+oneIntMatcher.group());
       }
     }
+    return result;
+  }
 
+  public static Integer firstPart(BufferedReader file) throws IOException {
+    Integer i, result = 0;
+    String line;
+
+    while((line = file.readLine()) != null) {
+      String first = "", last = "";
+      for (i=0; i<line.length(); i++) {
+        if (Character.isDigit(line.charAt(i))) {
+          if (first.isEmpty()) {
+            first = ""+line.charAt(i);
+          }
+          last = ""+line.charAt(i);
+        }
+      }
+      result += Integer.parseInt(first+last);
+    }
     return result;
   }
 
